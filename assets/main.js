009878f5795d7d805f7c72f9efb653ac7f9e0628
Justin Phillips <Jukphillips1990@gmail.com>
2023-08-23 03:06:41 -0600

    added js animations for about section when about btn is pressed

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,10 @@ const nameType = document.getElementById("nameType");
 const cursorType = document.getElementById("cursor")
 const about = document.getElementById('about')
 const links = document.getElementById('Links')
+const aboutHello = document.getElementById('aboutHello')
+const aboutStudy = document.getElementById('aboutStudy')
+const aboutWork = document.getElementById('aboutWork')
+const aboutText = document.getElementsByClassName('aboutText')
 // typewrite animation 
 
 function sleep(ms) {
@@ -28,4 +32,25 @@ const writeType = async () => {
     links.style.visibility = "visible";
 };
 
+function aboutSection() {
+
+    for(var i = 0; i < aboutText.length; i++){
+        
+        aboutText[i].style.animation = 'flySide 2s, fadeIn 2s';
+        aboutText[i].style.animationFillMode = 'forwards';
+        aboutText[i].style.animationDelay = (i * 2) + 's';
+        console.log(document.location)
+        // console.log(aboutText[i].style.animationDelay = (i * 2) + 's')
+    }
+    // create html about section 
+    // hide visibility
+    //use javascript to close whatever content is on the page and show about section
+    //switch "about" to "exit"
+}
+
 writeType();
+
+
+
+
+about.addEventListener("click", aboutSection)
